7a4dc1deb53452f7ecceb78bf7bed9d099eed384
Noam Kraiz <noamkraiz@MacBook-Air-sl-Noam.local>
2024-11-09 18:53:05 +0200

    Done

--- Bill3.java
+++ Bill3.java
@@ -7,11 +7,11 @@ public class Bill3 {
 		String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-		int price = Integer.parseInt(args[3]);
+		double price = Double.parseDouble(args[3]);
 		double dividdedPrice = price/3;
 		double roundPrice = Math.ceil(dividdedPrice);
 	    // System.out.println(" pay"+roundPrice+" Shekels each.");		
-	    System.out.println("Dear "+name1+" "+name2+" "+name3+": pay"+roundPrice+" Shekels each.");		
+	    System.out.println("Dear "+name3+", "+name2+", "+name1+": pay "+roundPrice+" Shekels each.");		
 
 	}
 }
